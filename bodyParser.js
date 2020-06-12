c52a8f3d3bea9c41e9fd42b7132d745e96bc7a7b
Kyrie Ving <834854385@qq.com>
2020-06-12 09:41:40 +0800

    feat：完善 parser

--- bodyParser.js
+++ bodyParser.js
@@ -10,13 +10,15 @@ function bodyParser(req, res) {
         res.setHeader("Content-type", "application/json");
         let postParam = "";
         req.on("data", (data) => {
-          // 得到的数据是 buffer 类型
+          // 得到的数据类型取决于传过来的body数据是什么
+          // 如果传入的body是字符串，这里得到的就是字符串
+          // 如果传入的body是对象或者数组，这里得到的就是buffer 需要使用arr.push(data), Buffer.concat(arr).toString("utf8") 得到 json 字符串，然后 JSON.parse()
+          // 这里只处理字符串类型，客户端body使用JSON.stringify()处理
           postParam += data;
         });
         req.on("end", () => {
           // json 字符串
-          const str = postParam.toString("utf8");
-          req.body = JSON.parse(str);
+          req.body = JSON.parse(postParam) || {};
           resolve();
         });
       } else {
